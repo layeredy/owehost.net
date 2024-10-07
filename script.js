155f67662e45fc3faa8e5a14143e7eba5c82f57d
Auri <auri@layeredy.com>
2024-10-07 17:52:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function reportAbuse() {
+  window.location.href = "https://billing.layeredy.com/supporttickets.php";
+}
+
+function goHome() {
+  window.location.href = "https://owehost.com";
+}
+
+function aup() {
+  window.location.href = "https://layeredy.com/aup";
+}
+// i have no idea why i coded this in javascript instead of html, but i did it anyways, made this like 4 months ago and never released it; edited on oct 7 2024 to include aup
